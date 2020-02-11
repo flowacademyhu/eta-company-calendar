6a07b9ed3efc9cee873b27bc7697d14735b5232e
Tamas Kocsis <kocsistomi.dm@gmail.com>
2020-02-11 14:29:28 +0100

    Módosítások: - Meeting és User entity-k Id GenerationType AUTO-ról IDENTITY-re - MeetingService-ben az @Autowired annotáció cserélve @AllArgsContsructor + private final-ra

--- eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/service/MeetingService.java
+++ eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/service/MeetingService.java
@@ -2,6 +2,7 @@ package hu.flowacademy.companycalendar.service;
 
 import hu.flowacademy.companycalendar.model.Meeting;
 import hu.flowacademy.companycalendar.repository.MeetingRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,10 +12,10 @@ import java.util.Optional;
 
 @Service
 @Transactional
+@AllArgsConstructor
 public class MeetingService {
 
-    @Autowired
-    private MeetingRepository meetingRepository;
+    private final MeetingRepository meetingRepository;
 
 
     public List<Meeting> findAll() {
