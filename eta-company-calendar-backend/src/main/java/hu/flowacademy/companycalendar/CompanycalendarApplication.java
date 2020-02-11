dfc22d03469b862156722df3c469190a0f789cb5
Robert Boros <robbie13.boros@gmail.com>
2020-02-11 09:06:42 +0100

    init backend

---
+++ eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/CompanycalendarApplication.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.companycalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanycalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanycalendarApplication.class, args);
+	}
+
+}
---
+++ eta-company-calendar-backend/src/test/java/hu/flowacademy/companycalendar/CompanycalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.companycalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanycalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
