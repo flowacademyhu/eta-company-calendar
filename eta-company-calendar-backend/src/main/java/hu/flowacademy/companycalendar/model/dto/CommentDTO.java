c78cf1267153ff9f3b6dfe8722260700730523bf
moldori <molnaardora@gmail.com>
2020-02-12 11:54:18 +0100

    fix typos and add exception

--- eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/model/dto/CommentDTO.java
+++ eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/model/dto/CommentDTO.java
@@ -19,35 +19,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class CommentDTO {
 
     private Long id;
-
     private String content;
-
     private Long userId;
-
     private Long meetingId;
-
 
     public CommentDTO(Comment comment) {
         this.id = comment.getId();
         this.content = comment.getContent();
         if (comment.getUser() != null) {
             this.userId = comment.getUser().getId();
-        } else
+        } else {
+            throw new RuntimeException("User not found with this id");
+        }
         if (comment.getMeeting() != null) {
             this.meetingId = comment.getMeeting().getId();
+        } else {
+            throw new RuntimeException("Meeting not found wit this id");
         }
     }
 
-
     public Comment toEntity(User user, Meeting meeting) {
-        return  new Comment(
+        return new Comment(
                 id,
                 content,
                 user,
                 meeting
         );
     }
-
-
-
 }
