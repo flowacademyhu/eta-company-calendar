5970e638c6198b3fe192929347da31f543261f41
Nóra Szűcs <56822114+noriszucs@users.noreply.github.com>
2020-02-12 10:43:30 +0100

    I16 reminder (#30)
    * add Reminder service, repository, model, dto, resource
    
    * add UserRepository and modified ReminderDTO
    
    * add create method to ReminderService
    
    * add reminder update method
    
    * add delete method and init dataloader
    
    * add postmanCollection to test directory
    
    * modified DTOs
    
    * add TODO to reminderService
    
    * remove comment

---
+++ eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/model/User.java
@@ -0,0 +1,20 @@
+package hu.flowacademy.companycalendar.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
