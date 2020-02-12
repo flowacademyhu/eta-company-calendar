7971310d69c6372fbec235303b7f06703f5485f0
Janos Mezo <jmezo1992@gmail.com>
2020-02-12 14:06:53 +0100

    added error handling to when the token expires

--- eta-company-calendar-frontend/src/app/shared/interceptors/auth-interceptor.service.ts
+++ eta-company-calendar-frontend/src/app/shared/interceptors/auth-interceptor.service.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler,
   HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
+import { Observable, throwError as observableThrowError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { ConfigurationService } from '../services/configuration.service';
 
@@ -11,13 +11,22 @@ export class AuthInterceptorService implements HttpInterceptor {
   constructor(private readonly config: ConfigurationService) { }
 
   public intercept(request: HttpRequest<object>, next: HttpHandler): Observable<HttpEvent<object>> {
-    if (!!this.config.fetchToken('access_token')) {
-      request = this.addToken(request, this.config.fetchToken('access_token'));
+    const accessToken = this.config.fetchToken('access_token');
+
+    if (!!accessToken) {
+      request = this.addToken(request, accessToken);
     }
+
     return next.handle(request)
     .pipe(
       catchError((error: HttpErrorResponse) => {
-        return throwError(error);
+        if (error.status === 401) {
+          const refreshToken = this.config.fetchToken('refreshToken');
+          if (!!refreshToken) {
+            this.config.clearToken();
+          }
+        }
+        return observableThrowError(error);
       }
     ));
   }
