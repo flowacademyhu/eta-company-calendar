36e46664917208b8bf1f5fe6a5f2c46be5ef6526
Jozsef Urban <jozsef.urban.88@gmail.com>
2020-02-12 21:51:36 +0100

    Builder added to Profile, JoinColumn annotation to user

--- eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/model/Profile.java
+++ eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/model/Profile.java
@@ -1,6 +1,7 @@
 package hu.flowacademy.companycalendar.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
@@ -9,6 +10,7 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @Table
 public class Profile {
@@ -17,6 +19,7 @@ public class Profile {
     private Long id;
 
     @OneToOne
+    @JoinColumn
     @MapsId
     private User user;
 
