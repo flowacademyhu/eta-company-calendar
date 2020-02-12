72e5dbb2817efd7fb7aabdb594139ce5e4f17105
Janos Mezo <jmezo1992@gmail.com>
2020-02-12 19:54:20 +0100

    Added i18n texts for login module. Moved error handling from auth service to login component.

--- eta-company-calendar-frontend/src/app/login/components/login.component.ts
+++ eta-company-calendar-frontend/src/app/login/components/login.component.ts
@@ -1,6 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { AuthService } from '~/app/shared/services/auth.service';
@@ -15,32 +17,25 @@ export class LoginComponent implements OnInit, OnDestroy {
   private loginForm: FormGroup;
   protected errorMessage: string;
 
-  constructor(private readonly auth: AuthService, private readonly router: Router) { }
+  constructor(private readonly auth: AuthService,
+              private readonly router: Router,
+              private readonly translate: TranslateService) { }
 
   public ngOnInit() {
     this.loginForm = new FormGroup({
       email: new FormControl(undefined, [Validators.email, Validators.required]),
       password: new FormControl(undefined, [Validators.required])
     });
-
-    // this.loginForm.setValue({
-    //   'email': 'admin1@test.com',
-    //   'password': 'admin123'
-    // });
   }
 
   protected onSubmit() {
-    if (this.loginForm.invalid || this.loginForm === undefined) {
-      this.errorMessage = 'form invalid';
-      return;
-    }
     const email = this.loginForm.get('email')?.value;
     const password = this.loginForm.get('password')?.value;
     this.auth.login(email, password)
     .pipe(takeUntil(this.destroy$))
     .subscribe(
       (_) => { this.router.navigate(['']); },
-      (error) => this.errorMessage = error
+      (error) => this.handleError(error)
     );
   }
 
@@ -50,6 +45,28 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   public ngOnDestroy() {
     this.destroy$.next(true);
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage;
+    if (!errorRes.error || ! errorRes.error.error) {
+      errorMessage = 'no_response';
+    } else {
+      switch (errorRes.error.error) {
+        case 'invalid_grant':
+          errorMessage = 'invalid_grant';
+          break;
+        default:
+          errorMessage = 'no_response';
+      }
+      this.setErrorMessage(`login.${errorMessage}`);
+    }
+  }
+
+  private setErrorMessage(messagePath: string) {
+    this.translate.get(messagePath)
+    .pipe(takeUntil(this.destroy$))
+    .subscribe((text) => this.errorMessage = text);
   }
 
 }
--- eta-company-calendar-frontend/src/app/shared/services/auth.service.ts
+++ eta-company-calendar-frontend/src/app/shared/services/auth.service.ts
@@ -1,7 +1,6 @@
-import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { AuthResponse } from '../models/auth-response.model';
 import { ApiCommunicationService } from './api-communication.service';
 import { ConfigurationService } from './configuration.service';
@@ -16,7 +15,6 @@ export class AuthService {
     return this.app.auth()
     .requestToken(email, password)
     .pipe(
-      catchError(this.handleError),
       tap((token) => {
         this.config.setToken(token);
       })
@@ -25,18 +23,6 @@ export class AuthService {
 
   public logout() {
     this.config.clearToken();
-  }
-
-  private handleError(errorRes: HttpErrorResponse) {
-    let errorMessage = 'The server is currently unavailable.';
-    if (!errorRes.error || ! errorRes.error.error) {
-      return throwError(errorMessage);
-    }
-    switch (errorRes.error.error) {
-      case 'invalid_grant':
-        errorMessage = 'Incorrect username or password!';
-    }
-    return throwError(errorMessage);
   }
 
 }
