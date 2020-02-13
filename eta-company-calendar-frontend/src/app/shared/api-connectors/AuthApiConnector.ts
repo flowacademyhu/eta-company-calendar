b745aca2f7e58ca319487b3d71096c968b3a1733
Janos Mezo <jmezo1992@gmail.com>
2020-02-13 15:19:08 +0100

    Moved refresh token to request body, created function for getting request headers.

--- eta-company-calendar-frontend/src/app/shared/api-connectors/AuthApiConnector.ts
+++ eta-company-calendar-frontend/src/app/shared/api-connectors/AuthApiConnector.ts
@@ -7,27 +7,25 @@ export class AuthApiConnector extends AbstractApiConnector {
   protected readonly apiRoute: string = `${this.apiBaseUrl}/oauth/token`;
 
   public requestToken(email: string, password: string): Observable<AuthResponse> {
-    const headers = new HttpHeaders({
-      'Authorization': 'Basic ' + btoa('fooClientIdPassword:secret'),
-      'Content-Type': 'application/x-www-form-urlencoded'
-    });
     const body = new HttpParams()
       .set('grant_type', 'password')
       .set('username', email)
       .set('password', password);
-
-    return this.http.post<AuthResponse>(this.apiRoute, body, { headers });
+    return this.http.post<AuthResponse>(this.apiRoute, body, { headers: this.getAuthHeaders() });
   }
 
   public refreshToken(token: string): Observable<AuthResponse> {
-    const headers = new HttpHeaders({
-      'Authorization': 'Basic ' + btoa('fooClientIdPassword:secret'),
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'refresh_token': token
-    });
     const body = new HttpParams()
-      .set('grant_type', 'refresh_token');
-    return this.http.post<AuthResponse>(this.apiRoute, body, { headers });
+      .set('grant_type', 'refresh_token')
+      .set('refresh_token', token);
+    return this.http.post<AuthResponse>(this.apiRoute, body, { headers: this.getAuthHeaders() });
+  }
+
+  private getAuthHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Authorization': 'Basic ' + btoa('fooClientIdPassword:secret'),
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
   }
 
 }
