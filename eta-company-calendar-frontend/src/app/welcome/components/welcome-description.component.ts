7ae757b01e9c45330672fe5dd56f8e4b1fdc3453
Jozsef Urban <jozsef.urban.88@gmail.com>
2020-02-14 15:28:13 +0100

    Merge frontend

--- eta-company-calendar-frontend/src/app/welcome/components/welcome-description.component.ts
+++ eta-company-calendar-frontend/src/app/welcome/components/welcome-description.component.ts
@@ -8,23 +8,7 @@ import { ApiCommunicationService } from '~/app/shared/services/api-communication
 
 @Component({
   selector: 'app-welcome-description',
-  template: `
-    <mat-toolbar color="primary" class="my-5">
-      {{'welcome.text' | translate}}
-      <button color="accent" mat-raised-button class="ml-auto"
-              (click)="onLanguageChange()">{{'welcome.button' | translate}}</button>
-    </mat-toolbar>
-
-    <mat-card *ngFor="let user of (users$ | async)" class="my-2">
-      <strong>{{user.name}}</strong><br>
-      {{user.email}}<br>
-      {{user.address.city}} - {{user.address.zipcode}} - {{user.address.street}} - {{user.address.suite}}<br>
-      {{user.phone}}<br>
-      {{user.website}}<br>
-    </mat-card>
-
-    <button mat-raised-button (click)="openDialog()" class="my-5">{{'newuserform.new_user' | translate}}</button>
-  `
+  template: ``
 })
 
 export class WelcomeDescriptionComponent {
