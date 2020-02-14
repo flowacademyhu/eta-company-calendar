e12935cfcbfcf20b1e73adaa98112769173cc4c9
Tamas Kocsis <kocsistomi.dm@gmail.com>
2020-02-14 08:40:13 +0100

    Added @Repository annotation to MeetingRepository

--- eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/repository/MeetingRepository.java
+++ eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/repository/MeetingRepository.java
@@ -1,5 +1,4 @@
 package hu.flowacademy.companycalendar.repository;
-
 import hu.flowacademy.companycalendar.model.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
