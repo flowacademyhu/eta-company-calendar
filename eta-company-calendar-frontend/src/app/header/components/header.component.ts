2ccc11835e02b9c9b495123f2d0455d9fed92afa
László Csányi <csalaoh@gmail.com>
2020-02-14 15:22:41 +0100

    modified routerLink

--- eta-company-calendar-frontend/src/app/header/components/header.component.ts
+++ eta-company-calendar-frontend/src/app/header/components/header.component.ts
@@ -11,7 +11,7 @@ import { ConfigurationService } from '../../shared/services/configuration.servic
   'button {border: 2px solid; border-color: black !important'],
   template:
   `<mat-toolbar class="my-0">
-    <a class="mr-3" mat-stroked-button routerLink=".">{{'header.profile' | translate}}</a>
+    <a class="mr-3" mat-stroked-button routerLink="profiles">{{'header.profile' | translate}}</a>
     <a class="mr-3" mat-stroked-button routerLink=".">{{'header.calendar' | translate}}</a>
     <a class="mr-3" mat-stroked-button routerLink=".">{{'header.meetings' | translate}}</a>
     <a class="mr-5"mat-stroked-button routerLink=".">{{'header.reminders' | translate}}</a>
