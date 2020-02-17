517e0bb8ef29ad6b511e7a692345673564b21ab0
Szűcs Nóra <norkaszucs@gmail.com>
2020-02-17 10:54:29 +0100

    add User Management button to nav bar

--- eta-company-calendar-frontend/src/app/header/components/header.component.ts
+++ eta-company-calendar-frontend/src/app/header/components/header.component.ts
@@ -8,13 +8,15 @@ import { ConfigurationService } from '../../shared/services/configuration.servic
   styles: [
     // tslint:disable-next-line: max-line-length
     'mat-toolbar { position: fixed; left: 0; top: 0; width: 100%; background-color: white; color: black; text-align: center}', 'p {cursor: pointer;}', 'a {border: 2px solid; border-color: black !important',
-  'button {border: 2px solid; border-color: black !important; width: 120px;' , 'a {width: 120px;'],
+    'button {border: 2px solid; border-color: black !important; width: 120px;' , 'a {width: 160px;'],
   template:
   `<mat-toolbar class="my-0">
     <a class="mr-3" mat-stroked-button routerLink="profiles">{{'header.profile' | translate}}</a>
     <a class="mr-3" mat-stroked-button routerLink=".">{{'header.calendar' | translate}}</a>
     <a class="mr-3" mat-stroked-button routerLink=".">{{'header.meetings' | translate}}</a>
-    <a class="mr-5"mat-stroked-button routerLink=".">{{'header.reminders' | translate}}</a>
+    <a class="mr-3" mat-stroked-button routerLink=".">{{'header.reminders' | translate}}</a>
+    <!-- TODO: add admin role (*ngIf) to User Management button -->
+    <a class="mr-5" mat-stroked-button routerLink=".">{{'header.userManagement' | translate}}</a>
     <p class="ml-auto" (click)="onLanguageChange()">{{'header.button' | translate}}</p>
     <button mat-stroked-button (click)="onLogout()" class="ml-3">{{'header.logout' | translate}}</button>
   </mat-toolbar>`
