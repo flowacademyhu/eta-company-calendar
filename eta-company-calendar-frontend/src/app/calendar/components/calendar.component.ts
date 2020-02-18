11617467d62e801303bf61c1e0b24b0663ee3ba2
Janos Mezo <jmezo1992@gmail.com>
2020-02-18 16:09:40 +0100

    fixed lint errors, added unsubscribe on component destroy

--- eta-company-calendar-frontend/src/app/calendar/components/calendar.component.ts
+++ eta-company-calendar-frontend/src/app/calendar/components/calendar.component.ts
@@ -1,13 +1,14 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
 import { FullCalendarComponent } from '@fullcalendar/angular';
 import { EventInput } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction'; // for dateClick
-import timeGrigPlugin from '@fullcalendar/timegrid';
-import { TranslateService } from '@ngx-translate/core';
-
 import enGbLocale from '@fullcalendar/core/locales/en-gb';
 import huLocale from '@fullcalendar/core/locales/hu';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGrigPlugin from '@fullcalendar/timegrid';
+import { TranslateService } from '@ngx-translate/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-calendar',
@@ -15,35 +16,38 @@ import huLocale from '@fullcalendar/core/locales/hu';
   templateUrl: 'calendar.component.html'
 })
 
-export class CalendarComponent implements AfterViewInit {
+export class CalendarComponent implements AfterViewInit, OnDestroy {
 
-  @ViewChild('calendar') public calendarComponent: FullCalendarComponent; // the #calendar in the template
+  private destroy$: Subject<boolean> = new Subject<boolean>();
+
+  @ViewChild('calendar') public calendarComponent: FullCalendarComponent;
+
+  public locales: object[] = [enGbLocale, huLocale];
 
   public calendarPlugins: object[] = [dayGridPlugin, timeGrigPlugin, interactionPlugin];
+
   private calendarEvents: EventInput[] = [
     { title: 'Event Now', start: new Date() },
   ];
 
   constructor(private readonly translate: TranslateService) {}
 
-  protected handleDateClick(arg) {
+  protected handleDateClick(arg: EventInput) {
     if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
-      this.calendarEvents = this.calendarEvents.concat({ // add new event data. must create new array
-        title: 'New Event',
+      this.calendarEvents = this.calendarEvents.concat({
+        allDay: arg.allDay,
         start: arg.date,
-        allDay: arg.allDay
-      })
+        title: 'New Event',
+      });
     }
   }
 
   public ngAfterViewInit() {
-    this.calendarComponent.getApi()
-      .setOption('locales', [huLocale, enGbLocale]);
-    this.calendarComponent.getApi()
-      .setOption('locale', 'en-gb');
-    this.translate.onLangChange.subscribe((params) => {
-      this.setCalendarLang(params.lang);
-    });
+    this.translate.onLangChange
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((params) => {
+        this.setCalendarLang(params.lang);
+      });
   }
 
   private setCalendarLang(lang: string) {
@@ -52,6 +56,10 @@ export class CalendarComponent implements AfterViewInit {
     }
     this.calendarComponent.getApi()
       .setOption('locale', lang);
+  }
+
+  public ngOnDestroy() {
+    this.destroy$.next(true);
   }
 
 }
