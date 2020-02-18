92a81eacae481b51a90f61eba0f0db775c477150
Janos Mezo <jmezo1992@gmail.com>
2020-02-18 15:12:33 +0100

    changed calendar language to hu

--- eta-company-calendar-frontend/src/app/calendar/components/calendar.component.ts
+++ eta-company-calendar-frontend/src/app/calendar/components/calendar.component.ts
@@ -1,9 +1,11 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { FullCalendarComponent } from '@fullcalendar/angular';
 import { EventInput } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction'; // for dateClick
 import timeGrigPlugin from '@fullcalendar/timegrid';
+
+import huLocale from '@fullcalendar/core/locales/hu';
 
 @Component({
   selector: 'app-calendar',
@@ -11,31 +13,21 @@ import timeGrigPlugin from '@fullcalendar/timegrid';
   templateUrl: 'calendar.component.html'
 })
 
-export class CalendarComponent {
+export class CalendarComponent implements AfterViewInit {
 
-  @ViewChild('calendar') calendarComponent: FullCalendarComponent; // the #calendar in the template
+  @ViewChild('calendar') public calendarComponent: FullCalendarComponent; // the #calendar in the template
 
-  calendarVisible = true;
   calendarPlugins = [dayGridPlugin, timeGrigPlugin, interactionPlugin];
-  calendarWeekends = true;
-  calendarEvents: EventInput[] = [
-    { title: 'Event Now', start: new Date() }
+  private calendarEvents: EventInput[] = [
+    { title: 'Event Now', start: new Date() },
   ];
-
-  toggleVisible() {
-    this.calendarVisible = !this.calendarVisible;
-  }
-
-  toggleWeekends() {
-    this.calendarWeekends = !this.calendarWeekends;
-  }
 
   gotoPast() {
     let calendarApi = this.calendarComponent.getApi();
     calendarApi.gotoDate('2000-01-01'); // call a method on the Calendar object
   }
 
-  handleDateClick(arg) {
+  protected handleDateClick(arg) {
     if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
       this.calendarEvents = this.calendarEvents.concat({ // add new event data. must create new array
         title: 'New Event',
@@ -43,6 +35,13 @@ export class CalendarComponent {
         allDay: arg.allDay
       })
     }
+  }
+
+  public ngAfterViewInit() {
+    this.calendarComponent.locales = [huLocale];
+    this.calendarComponent.locale = 'hu';
+    this.calendarComponent.getApi()
+    .setOption('locale', 'hu');
   }
 
 }
