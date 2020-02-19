cc89b68d715a1bda67d569c8e7337e15c9f69964
László Csányi <csalaoh@gmail.com>
2020-02-19 12:12:19 +0100

    EmialService refactor

--- eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/email/EmailService.java
+++ eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/email/EmailService.java
@@ -1,6 +1,5 @@
 package hu.flowacademy.companycalendar.email;
 
 public interface EmailService {
-    void sendText(String from, String to, String subject, String body);
-    void sendHTML(String from, String to, String subject, String body);
+    void send(String to, String subject, EmailType emailType, Object... bodyParams);
 }
--- eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/email/MailGunEmailService.java
+++ eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/email/MailGunEmailService.java
@@ -1,80 +1,50 @@
 package hu.flowacademy.companycalendar.email;
 
 import hu.flowacademy.companycalendar.config.mailing.MailingConfig;
-import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.PostConstruct;
-
+@Slf4j
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class MailGunEmailService implements EmailService {
-    private final MailingConfig mailingConfig;
+    @NonNull private final MailingConfig mailingConfig;
+    @NonNull private final EmailFactory emailFactory;
     private final RestTemplate restTemplate = new RestTemplate();
 
     @Override
-    public void sendText(String from, String to, String subject, String body) {
-        MultiValueMap<String, String> map = getPostRequestObject(from, to, subject);
-        map.add("text", body);
+    public void send(String to, String subject, EmailType emailType, Object... bodyParams) {
+        asyncSend(emailFactory.buildEmail(mailingConfig.getMailFrom(), to, subject, emailType, bodyParams), createHeader());
     }
 
-    @Override
-    public void sendHTML(String from, String to, String subject, String body) {
-        MultiValueMap<String, String> map = getPostRequestObject(from, to, subject);
-        map.add("html", body);
-    }
-
-    //@PostConstruct
-    private void sendEmail(String to, String subject, String body) {
-        String user = "Feri";
-        String startTime = "14:00";
-        String finishTime = "16:00";
-        String meetingRoom = "1. sz.";
-
-        // üzenet összerakása
-        String message = String.format(
-                mailingConfig.getMessageTemplate(),
-                user,
-                startTime,
-                finishTime,
-                meetingRoom
-        ); // helyettesíthető MessageFormat-tal "Hello {0}, értekezleted van {1} ... "
-
-        // HTTP header összerakása: basic auth és contentType megadása
+    private HttpHeaders createHeader() {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         headers.setBasicAuth("api", mailingConfig.getMailgun().getApiKey());
+        return headers;
+    }
 
-        // form data megadása
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add("from", mailingConfig.getMailFrom());
-        map.add("to", to);
-        map.add("subject", subject);
-        map.add("text", message); // csere body-ra
-
-        // http entity (request) object összerakása
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
-
-        // kérés elküldése
-        ResponseEntity<String> response = restTemplate.postForEntity(mailingConfig.getMailgun().getMailSenderUrl(),
+    @Async
+    private void asyncSend(MultiValueMap<String, String> body, HttpHeaders headers) {
+        try {
+            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
+            ResponseEntity<String> response = restTemplate.postForEntity(mailingConfig.getMailgun().getMailSenderUrl() + "/messages",
                 request, String.class);
 
-        System.out.println(response);
-    }
+            log.info("Mail send with request [{}] and got [{}]", request, response);
+        } catch (Throwable t) {
+            log.error("Error when email sending", t);
+        }
 
-    private MultiValueMap<String, String> getPostRequestObject(String from, String to, String subject) {
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
-        map.add("from", from);
-        map.add("to", to);
-        map.add("subject", subject);
-        return map;
     }
-
 }
