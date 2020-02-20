b30699262d448e158cc2ac70c16db7615f0c46a6
Janos Mezo <jmezo1992@gmail.com>
2020-02-20 20:24:50 +0100

    added dummy users and meetings to db for testing

--- eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/utils/InitDataLoader.java
+++ eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/utils/InitDataLoader.java
@@ -6,6 +6,7 @@ import hu.flowacademy.companycalendar.model.Recurring;
 import hu.flowacademy.companycalendar.model.User;
 import hu.flowacademy.companycalendar.repository.MeetingRepository;
 import hu.flowacademy.companycalendar.repository.UserRepository;
+import java.util.ArrayList;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -32,20 +33,26 @@ public class InitDataLoader {
     }
 
     private void createUsers() {
+        List<User> testUsers = new ArrayList<>();
         var admin = User.builder()
                 .email("admin1@test.com")
                 .password(passwordEncoder.encode("admin123"))
                 .role(Roles.ADMIN)
                 .build();
-        var user = User.builder()
-                .email("user1@test.com")
+        testUsers.add(admin);
+        for(int i = 0; i < 10; i++) {
+            var user = User.builder()
+                .email("user" + i + "@test.com")
                 .password(passwordEncoder.encode("user123"))
                 .role(Roles.USER)
                 .build();
-        userRepository.saveAll(List.of(admin, user));
+            testUsers.add(user);
+        }
+        userRepository.saveAll(testUsers);
     }
 
     private void createMeetings() {
+        var testUsers = userRepository.findAll();
         meetingRepository.save(Meeting.builder()
             .title("First_testMeeting")
             .description("TestDescription")
@@ -64,5 +71,21 @@ public class InitDataLoader {
             .finishTime(System.currentTimeMillis() + 3600000 * 2)
             .createdAt(System.currentTimeMillis())
             .build());
+        List<Meeting> testMeetings = new ArrayList<>();
+        for(int i = 0; i < 10; i++) {
+            var meeting = Meeting.builder()
+                .title("test meeting " + i)
+                .description("description of test meeting " + i)
+                .location(Location.MEETING_ROOM)
+                .startingTime(System.currentTimeMillis() + 3600000)
+                .finishTime(System.currentTimeMillis() + 3600000 * 2)
+                .createdAt(System.currentTimeMillis())
+                .createdBy(testUsers.get(i))
+                .requiredAttendants(testUsers.subList(i + 1, 10))
+                .optionalAttendants(List.of(testUsers.get(0)))
+                .build();
+            testMeetings.add(meeting);
+        }
+        meetingRepository.saveAll(testMeetings);
     }
 }
