7cc8bdacd46cfd1a88460045e6acd72d821e1ecf
moldori <molnaardora@gmail.com>
2020-02-20 11:48:16 +0100

    fix typos

--- eta-company-calendar-frontend/src/app/user-management/components/user-management-description.component.ts
+++ eta-company-calendar-frontend/src/app/user-management/components/user-management-description.component.ts
@@ -7,6 +7,9 @@ import { NewUserComponent } from '~/app/user-management/modals/new-user.componen
   styles: [`.sidenav-container {
     height: 500px;
   }
+    mat-sidenav {
+      width: 220px
+    }
   `,
   ],
   template: `
