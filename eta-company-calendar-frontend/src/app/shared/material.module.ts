4a3e2034f920264aba97adcad08a100db629e01e
Plesa <mbp@mbps-MacBook-Pro.local>
2020-02-21 13:05:16 +0100

    travis problems fix

--- eta-company-calendar-frontend/src/app/shared/material.module.ts
+++ eta-company-calendar-frontend/src/app/shared/material.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule, MatListModule, MatSidenavModule } from '@angular/material';
+import { MatMenuModule } from '@angular/material';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatMenuModule } from '@angular/material'
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
