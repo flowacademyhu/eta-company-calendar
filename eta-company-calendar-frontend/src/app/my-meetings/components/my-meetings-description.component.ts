4537a8d1d8780ee4283abdb17cd765b9afb12ef6
Tamas Kocsis <kocsistomi.dm@gmail.com>
2020-02-21 08:14:37 +0100

    Columns of meeting-list table are showing relevant details

--- eta-company-calendar-frontend/src/app/my-meetings/components/my-meetings-description.component.ts
+++ eta-company-calendar-frontend/src/app/my-meetings/components/my-meetings-description.component.ts
@@ -5,50 +5,64 @@ import { MeetingService } from '~/app/my-meetings/service/meeting.service';
 
 @Component({
   selector: 'app-my-meetings-description',
+  styles: ['table { width: 85%; }', 'th.mat-header-cell {text-align: center;}', 'td.mat-cell {text-align: center;}' ],
   template: `
-	<div class="row justify-content-center">
-      <table mat-table [dataSource]="meetings$ | async" class="mat-elevation-z8">
-        <ng-container matColumnDef="id">
-          <th mat-header-cell *matHeaderCellDef class="center">id</th>
-          <td mat-cell  *matCellDef="let meeting"> {{meeting.id}} </td>
-        </ng-container>
+	<div class="row justify-content-center" mt-4>
+  <table mat-table [dataSource]="meetings$ | async" class="mat-elevation-z8">
 
-				<ng-container matColumnDef="title">
-          <th mat-header-cell *matHeaderCellDef class="center">title</th>
-          <td mat-cell *matCellDef="let meeting">{{meeting.title}}</td>
-        </ng-container>
+  <ng-container matColumnDef="id">
+    <th mat-header-cell *matHeaderCellDef class="text-center">{{ 'meetinglist.id' | translate }}</th>
+    <td mat-cell  *matCellDef="let meeting">{{meeting.id}}</td>
+  </ng-container>
 
-				<ng-container matColumnDef="description">
-          <th mat-header-cell *matHeaderCellDef class="center">Desc</th>
-          <td mat-cell *matCellDef="let meeting">{{meeting.description}}</td>
-        </ng-container>
+  <ng-container matColumnDef="title">
+    <th mat-header-cell *matHeaderCellDef class="text-center">{{ 'meetinglist.title' | translate }}</th>
+    <td mat-cell *matCellDef="let meeting">{{meeting.title}}</td>
+  </ng-container>
 
-				<ng-container matColumnDef="action">
-          <th mat-header-cell *matHeaderCellDef></th>
-          <td mat-cell *matCellDef="let meeting">
-					<button mat-icon-button>
-					<mat-icon aria-label="User">
-            edit
-          </mat-icon>
-					</button>
-					<button mat-icon-button>
-          <mat-icon aria-label="Delete Icon">
-            delete
-          </mat-icon>
-          </button>
-          </td>
-        </ng-container>
+  <ng-container matColumnDef="date">
+    <th mat-header-cell *matHeaderCellDef class="text-center">{{ 'meetinglist.date' | translate }}</th>
+    <td mat-cell *matCellDef="let meeting">{{meeting.startingTime | date: 'yyyy-MM-dd'}}</td>
+  </ng-container>
 
-				<tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
-        <tr mat-row *matRowDef="let row; columns: displayedColumns;" align="center" ></tr>
-      </table>
-    </div>
+  <ng-container matColumnDef="startingTime">
+    <th mat-header-cell *matHeaderCellDef class="text-center">{{ 'meetinglist.startingTime' | translate }}</th>
+    <td mat-cell *matCellDef="let meeting">{{meeting.startingTime | date: 'HH:mm'}}</td>
+  </ng-container>
+
+  <ng-container matColumnDef="finishTime">
+    <th mat-header-cell *matHeaderCellDef class="text-center">{{ 'meetinglist.finishTime' | translate }}</th>
+    <td mat-cell *matCellDef="let meeting">{{meeting.finishTime | date: 'HH:mm'}}</td>
+  </ng-container>
+
+  <ng-container matColumnDef="action">
+    <th mat-header-cell *matHeaderCellDef class="text-center"></th>
+    <td mat-cell *matCellDef="let meeting">
+
+    <button mat-icon-button>
+		<mat-icon aria-label="Edit meeting">
+         edit
+       </mat-icon>
+		</button>
+
+    <button mat-icon-button>
+       <mat-icon aria-label="Delete meeting">
+         delete
+       </mat-icon>
+       </button>
+       </td>
+     </ng-container>
+
+  <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+  <tr mat-row *matRowDef="let row; columns: displayedColumns;" align="center" ></tr>
+ </table>
+ </div>
   `,
 })
 export class MyMeetingsDescriptionComponent implements OnInit {
 
   protected meetings$: Observable<Meeting[]>;
-  public displayedColumns: string[] = ['id', 'title', 'description', 'action'];
+  public displayedColumns: string[] = ['id', 'title', 'date', 'startingTime', 'finishTime',  'action'];
 
   constructor(private readonly meetingService: MeetingService) {}
 
