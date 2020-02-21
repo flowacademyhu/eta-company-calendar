6270e0ce5acffa083f72ce756b092ce070c44ae5
csalaoh <40830392+csalaoh@users.noreply.github.com>
2020-02-21 12:34:48 +0100

    I57 view profile information (#82)
    * Profil view component
    
    * pipe
    
    * translate works
    
    * one dialog for view and edit
    
    * Edit profil data
    
    * deleted profil-edit-dialog
    
    * date bnding not work
    
    * fixing bugs
    
    * delete htmlcomponent
    
    * profil view and modify
    
    * linter check
    
    * ngmodel -> reactive form
    
    * profile data view and edit (reactive form)
    
    * done

--- eta-company-calendar-frontend/src/app/shared/shared.module.ts
+++ eta-company-calendar-frontend/src/app/shared/shared.module.ts
@@ -8,22 +8,28 @@ import { MaterialModule } from '~/app/shared/material.module';
 import { MainLayoutComponent } from '~/app/shared/pages/main-layout.component';
 import { ApiCommunicationService } from '~/app/shared/services/api-communication.service';
 import { ConfigurationService } from '~/app/shared/services/configuration.service';
-import { FooterComponent} from '../footer/components/footer.component';
-import { HeaderComponent } from '../header/components/header.component';
+import { FooterComponent } from '../footer/components/footer.component';
+import { HeaderComponent } from '../header/components/HeaderComponent';
 
 import { AuthGuard } from './guards/auth.guard';
 import { AuthInterceptorService } from './interceptors/auth-interceptor.service';
+import { ProfilViewDialog } from './modals/profil-view-dialog.component';
 @NgModule({
   declarations: [
     MainLayoutComponent,
     HeaderComponent,
     FooterComponent,
+    ProfilViewDialog,
+  ],
+  entryComponents: [
+    ProfilViewDialog,
   ],
   exports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     MaterialModule,
+    ProfilViewDialog,
   ],
   imports: [
     CommonModule,
@@ -37,7 +43,7 @@ import { AuthInterceptorService } from './interceptors/auth-interceptor.service'
     ApiCommunicationService,
     ConfigurationService,
     AuthGuard,
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true},
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true },
   ],
 })
 export class SharedModule { }
