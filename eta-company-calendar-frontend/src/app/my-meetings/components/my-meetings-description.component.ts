fec7ab76b1fdbbdc464c9b21c07ff0bc873e3ac1
Tamas Kocsis <kocsistomi.dm@gmail.com>
2020-02-21 11:42:52 +0100

    Minor modifications

--- eta-company-calendar-frontend/src/app/my-meetings/components/my-meetings-description.component.ts
+++ eta-company-calendar-frontend/src/app/my-meetings/components/my-meetings-description.component.ts
@@ -5,7 +5,8 @@ import { MeetingService } from '~/app/my-meetings/service/meeting.service';
 
 @Component({
   selector: 'app-my-meetings-description',
-  styles: ['table { width: 85%; }',
+  styles: [
+  'table { width: 70%; }',
   'th.mat-header-cell {text-align: center;}',
   'td.mat-cell {text-align: center;}',
 ],
@@ -33,17 +34,41 @@ import { MeetingService } from '~/app/my-meetings/service/meeting.service';
     <td mat-cell *matCellDef="let meeting">{{meeting.title}}</td>
   </ng-container>
 
-  <ng-container matColumnDef="action">
+  <ng-container matColumnDef="description">
     <th mat-header-cell *matHeaderCellDef class="text-center">{{ 'meetinglist.description' | translate }}</th>
     <td mat-cell *matCellDef="let meeting">
 
     <button mat-icon-button matTooltip="{{meeting.description}}">
-		<mat-icon aria-label="{{'meetinglist.description' | translate}}">
+		  <mat-icon>
          library_books
-       </mat-icon>
+      </mat-icon>
     </button>
-       </td>
-     </ng-container>
+    </td>
+  </ng-container>
+
+  <ng-container matColumnDef="reqAttendants">
+    <th mat-header-cell *matHeaderCellDef class="text-center">{{ 'meetinglist.reqAttendants' | translate }}</th>
+    <td mat-cell *matCellDef="let meeting">
+
+    <button mat-icon-button matTooltip="{{meeting.requiredAttendants}}">
+		  <mat-icon>
+         people
+      </mat-icon>
+    </button>
+    </td>
+  </ng-container>
+
+  <ng-container matColumnDef="optAttendants">
+    <th mat-header-cell *matHeaderCellDef class="text-center">{{ 'meetinglist.optAttendants' | translate }}</th>
+    <td mat-cell *matCellDef="let meeting">
+
+    <button mat-icon-button matTooltip="{{meeting.optionalAttendants}}">
+		  <mat-icon>
+         people_outline
+      </mat-icon>
+    </button>
+    </td>
+  </ng-container>
 
   <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
   <tr mat-row *matRowDef="let row; columns: displayedColumns;" align="center" ></tr>
@@ -54,7 +79,7 @@ import { MeetingService } from '~/app/my-meetings/service/meeting.service';
 export class MyMeetingsDescriptionComponent implements OnInit {
 
   protected meetings$: Observable<Meeting[]>;
-  public displayedColumns: string[] = ['date', 'startingTime', 'finishTime', 'title', 'action'];
+  public displayedColumns: string[] = ['date', 'startingTime', 'finishTime', 'title', 'description', 'reqAttendants', 'optAttendants'];
 
   constructor(private readonly meetingService: MeetingService) {}
 
