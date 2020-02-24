62c53efdd834dd5bbc98c98754afeda074e4770d
Janos Mezo <jmezo1992@gmail.com>
2020-02-24 13:33:47 +0100

    removed repository call

--- eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/config/CustomTokenEnhancer.java
+++ eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/config/CustomTokenEnhancer.java
@@ -1,14 +1,17 @@
 package hu.flowacademy.companycalendar.config;
 
+import hu.flowacademy.companycalendar.model.User;
 import hu.flowacademy.companycalendar.repository.UserRepository;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
+@Slf4j
 public class CustomTokenEnhancer implements TokenEnhancer {
 
   @Autowired
@@ -17,10 +20,14 @@ public class CustomTokenEnhancer implements TokenEnhancer {
   @Override
   public OAuth2AccessToken enhance(OAuth2AccessToken token,
       OAuth2Authentication auth) {
-    Map<String, Object> additionalInfo = new HashMap<>();
-    additionalInfo.put("id", userRepository.findFirstByEmail(auth.getName()).orElseThrow().getId());
-    ((DefaultOAuth2AccessToken) token).setAdditionalInformation(
-        additionalInfo);
-    return token;
+    Object principal = auth.getPrincipal();
+    if (principal instanceof User) {
+      var user = (User) principal;
+      ((DefaultOAuth2AccessToken) token).setAdditionalInformation(
+          Map.of("id", user.getId()));
+      return token;
+    }
+    log.error("Principal is not User.class");
+   return token;
   }
 }
