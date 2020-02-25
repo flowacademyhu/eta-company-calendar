ef2bc6697f1d7d76f42c2539830884fabc6d8b79
Tamas Kocsis <kocsistomi.dm@gmail.com>
2020-02-25 23:27:06 +0100

    Fixed linting problems

--- eta-company-calendar-frontend/src/app/shared/material.module.ts
+++ eta-company-calendar-frontend/src/app/shared/material.module.ts
@@ -10,13 +10,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [],
