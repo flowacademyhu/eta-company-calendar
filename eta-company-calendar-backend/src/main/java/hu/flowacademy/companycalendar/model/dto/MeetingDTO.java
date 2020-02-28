95dd3252f88d4f17b94b3ea276480361c32003fa
Jozsef Urban <jozsef.urban.88@gmail.com>
2020-02-28 14:12:33 +0100

    backend meetingDTO modified

--- eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/model/dto/MeetingDTO.java
+++ eta-company-calendar-backend/src/main/java/hu/flowacademy/companycalendar/model/dto/MeetingDTO.java
@@ -1,9 +1,11 @@
 package hu.flowacademy.companycalendar.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import hu.flowacademy.companycalendar.model.Location;
 import hu.flowacademy.companycalendar.model.Meeting;
 import hu.flowacademy.companycalendar.model.Recurring;
 import hu.flowacademy.companycalendar.model.User;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,41 +22,47 @@ import java.util.List;
 @NoArgsConstructor
 public class MeetingDTO {
 
-    private Long id;
-    private String title;
-    private String description;
-    private Location location;
-    private String otherLocation;
-    private Recurring recurring;
-    private Long startingTime;
-    private Long finishTime;
-    private User createdBy;
-    private User updatedBy;
-    private Long createdAt;
-    private Long updatedAt;
-    private List<User> requiredAttendants;
-    private List<User> optionalAttendants;
+  private Long id;
+  private String title;
+  private String description;
+  private Location location;
+  private String otherLocation;
+  private Recurring recurring;
+  private Long startingTime;
+  private Long finishTime;
+  private UserResponseDTO createdBy;
+  private UserResponseDTO updatedBy;
+  private Long createdAt;
+  private Long updatedAt;
+  private List<UserResponseDTO> requiredAttendants;
+  private List<UserResponseDTO> optionalAttendants;
 
-    public MeetingDTO(Meeting meeting) {
-        this.id = meeting.getId();
-        this.title = meeting.getTitle();
-        this.description = meeting.getDescription();
-        this.location = meeting.getLocation();
-        this.otherLocation = meeting.getOtherLocation();
-        this.recurring = meeting.getRecurring();
-        this.startingTime = meeting.getStartingTime();
-        this.finishTime = meeting.getFinishTime();
-        this.createdBy = meeting.getCreatedBy();
-        this.updatedBy = meeting.getUpdatedBy();
-        this.createdAt = meeting.getCreatedAt();
-        this.updatedAt = meeting.getUpdatedAt();
-        this.requiredAttendants = meeting.getRequiredAttendants();
-        this.optionalAttendants = meeting.getOptionalAttendants();
+  public MeetingDTO(Meeting meeting) {
+    this.id = meeting.getId();
+    this.title = meeting.getTitle();
+    this.description = meeting.getDescription();
+    this.location = meeting.getLocation();
+    this.otherLocation = meeting.getOtherLocation();
+    this.recurring = meeting.getRecurring();
+    this.startingTime = meeting.getStartingTime();
+    this.finishTime = meeting.getFinishTime();
+    this.createdBy = new UserResponseDTO(meeting.getCreatedBy());
+    if (meeting.getUpdatedBy() != null) {
+      this.updatedBy = new UserResponseDTO(meeting.getUpdatedBy());
     }
+    this.createdAt = meeting.getCreatedAt();
+    this.updatedAt = meeting.getUpdatedAt();
+    this.requiredAttendants = meeting.getRequiredAttendants().stream().map(UserResponseDTO::new)
+        .collect(
+            Collectors.toList());
+    this.optionalAttendants = meeting.getOptionalAttendants().stream().map(UserResponseDTO::new)
+        .collect(
+            Collectors.toList());
+  }
 
-    public Meeting toEntity() {
-        Meeting meeting = new Meeting();
-        BeanUtils.copyProperties(this, meeting);
-        return meeting;
-    }
+  public Meeting toEntity() {
+    Meeting meeting = new Meeting();
+    BeanUtils.copyProperties(this, meeting);
+    return meeting;
+  }
 }
