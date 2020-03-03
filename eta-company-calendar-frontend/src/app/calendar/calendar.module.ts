181a397e7d87b30e1fde431259c7927c12664e60
Janos Mezo <jmezo1992@gmail.com>
2020-03-03 17:03:04 +0100

    added attendant component to module

--- eta-company-calendar-frontend/src/app/calendar/calendar.module.ts
+++ eta-company-calendar-frontend/src/app/calendar/calendar.module.ts
@@ -6,7 +6,7 @@ import { MeetingService } from '../my-meetings/service/meeting.service';
 import { ReminderService } from '../reminder/service/reminder.service';
 import { SharedModule } from '../shared/shared.module';
 import { CalendarRoutingModule } from './calendar-routing.module';
-// import { AttendantsComponent } from './components/attendants.component';
+import { AttendantsComponent } from './components/attendants.component';
 import { CalendarComponent } from './components/calendar.component';
 import { RecurrenceShowComponent } from './components/recurrence-show.component';
 import { EventReminderSelectorComponent } from './modals/event-reminder-selector.component';
@@ -22,6 +22,7 @@ import { ReminderCreateComponent } from './modals/reminder-create.component';
     RecurrenceShowComponent,
     EventReminderSelectorComponent,
     ReminderCreateComponent,
+    AttendantsComponent,
   ],
   entryComponents: [
     MeetingCreateComponent,
