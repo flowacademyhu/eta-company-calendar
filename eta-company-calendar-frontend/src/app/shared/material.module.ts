d56d9aed0733f784a7b40f3f2487472855658407
TamasPlesa <47762801+TamasPlesa@users.noreply.github.com>
2020-03-04 16:15:34 +0100

    modified dialog (#209)

--- eta-company-calendar-frontend/src/app/shared/material.module.ts
+++ eta-company-calendar-frontend/src/app/shared/material.module.ts
@@ -12,7 +12,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatRadioModule } from '@angular/material/radio';
+import { MAT_RADIO_DEFAULT_OPTIONS, MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -74,7 +74,10 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatPaginatorModule,
     MatAutocompleteModule,
   ],
-
+  providers: [{
+    provide: MAT_RADIO_DEFAULT_OPTIONS,
+    useValue: { color: 'primary' },
+}]
 })
 
 export class MaterialModule { }
